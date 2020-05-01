5b1b18d6c9ed40f4c9199684954b38efc61591f5
cuchipkg <60688301+cuchipkg@users.noreply.github.com>
2020-05-01 22:56:39 +0700

    Update binarysearch.rb

--- binarysearch.rb
+++ binarysearch.rb
@@ -1,13 +1,13 @@
 
-def BinaryTree(array,left,right,vsearch)
-	mid = (left+right)/2
+def BinarySearch(array,left,right,vsearch)#de qui
+	mid = (left+right)/2 #cong dau cuoi chia 2 de so sanh gia tri giua voi gia tri can tim
 	printf "Left: %d Right %d,Mid %d\n",left,right,mid
 	if vsearch == array[mid]
 		printf "The Value %d At %d \n",vsearch,mid
 	elsif vsearch > array[mid]
-		return BinaryTree(array,mid+1,right,vsearch)
+		return BinarySearch(array,mid+1,right,vsearch)#neu gia tri can tim lon hon,tang chi muc len 1
 	elsif vsearch < array[mid]
-		return BinaryTree(array,left,mid-1,vsearch)
+		return BinarySearch(array,left,mid-1,vsearch)#neu gia tri can tim be hon, giam chi muc xuong 1
 	else
 		return -1
 	end
@@ -16,4 +16,4 @@ mang = [1,12,33,40,60,80,90,91,95,100]
 puts "Nhap Gia tri can tim"
 search = gets.chomp.to_i
 
-puts BinaryTree(mang,0,mang.size,search)
+puts BinarySearch(mang,0,mang.size,search)
